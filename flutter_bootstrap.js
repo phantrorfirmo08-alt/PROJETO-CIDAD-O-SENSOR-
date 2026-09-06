71c82caa28b8047da813393d664c8950b2f94069
phantrorfirmo08-alt <phantrorfirmo08-alt@users.noreply.github.com>
2026-09-06 18:53:34 +0000

    deploy: f0365ef918e3378f1376e452b13ddba967a29401

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "288775727" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "648825144" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
